d8f0c0073c8b1d331fed0396e483ac75225db9fe
mbordon01 <martibordon31@gmail.com>
2025-03-31 12:06:50 -0300

    cambio2

--- pryBordonGit1/Program.cs
+++ pryBordonGit1/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("¡Primer cambio cambio en el código, subiéndo a GitHub!");
 Console.WriteLine("¡Segundo cambio cambio en el código, subiéndo a GitHub!");
